58ec0f1fd6cdf34b7a8aefbe368fec7c7dfcd049
Andrew Hearin <andrew.hearin@yale.edu>
2016-08-17 10:45:01 -0400

    Added command-line script

---
+++ reduce_sfh_ascii.py
@@ -0,0 +1,38 @@
+""" Command line script to write reduced versions of sfh ascii data
+"""
+import os
+import read_sfh
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument("fname", type=str,
+    help="Filename storing the entire sfh file")
+
+parser.add_argument("log10_sm_cut", type=float,
+    help="stellar mass cut in log10 units")
+
+# parser.add_argument("-overwrite",
+#     help="Overwrite the existing data file (if present). ",
+#     default=False, type=bool)
+
+parser.add_argument("-output_dirname",
+    help="Disk location to store the reduced catalog",
+    default=None, type=str)
+
+parser.add_argument("-history",
+    help="star-formation vs. ICL history",
+    choices=['sfh', 'iclh'], default='sfh', type=str)
+
+
+args = parser.parse_args()
+stellar_mass_cut = 10**args.log10_sm_cut
+if args.output_dirname is None:
+    output_dirname = os.path.dirname(args.fname)
+else:
+    output_dirname = args.output_dirname
+history = args.history
+input_fname = args.fname
+
+output_fname = read_sfh.get_reduced_filename(input_fname, stellar_mass_cut,
+        output_dirname=output_dirname, history=history)
+read_sfh.write_reduced_history(input_fname, stellar_mass_cut, output_fname, history=history)
