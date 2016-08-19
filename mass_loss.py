7ca9007d72d34d65d48585a08f399fed337080d9
Andrew Hearin <andrew.hearin@yale.edu>
2016-08-19 13:11:10 -0400

    Added redshift_zero_stellar_mass function. Agrees with Behroozi tabulation within 1%

--- mass_loss.py
+++ mass_loss.py
@@ -1,17 +1,25 @@
 """
 """
 import numpy as np
+from read_sfh import times as universe_age_array
+
+universe_dt_array = np.diff(np.insert(universe_age_array, 0, 0))
 
 
 def mass_loss_fraction(x):
     """
     """
-    return 0.05*np.log(1 + x/1.4)
+    return 0.05*np.log(1 + 1000.*x/1.4)
 
 
-def remaining_mass(t_now, sfh, t):
+def redshift_zero_stellar_mass(sfr_histories):
     """
     """
-    x = t_now - t
-    frac_loss = mass_loss_fraction(x)
-    integrand = sfh * (1. - frac_loss)
+    shape_err_msg = "Input ``sfr_histories`` must have shape (num_gals, num_time_steps)"
+    assert len(np.shape(sfr_histories)) == 2, shape_err_msg
+
+    frac_loss = mass_loss_fraction(universe_age_array[-1] - universe_age_array)
+    integrand = sfr_histories * (1. - frac_loss)
+    z0_stellar_mass = np.sum(integrand*universe_dt_array, axis=1)*1e9
+
+    return z0_stellar_mass
