0bfcbc8c3640e5ce1b0aa839d9f33705c99111e9
Andrew Hearin <andrew.hearin@yale.edu>
2016-08-19 17:40:16 -0400

    Developing quenching time calculatino

---
+++ quenching_time.py
@@ -0,0 +1,21 @@
+"""
+"""
+import numpy as np
+
+
+def quenching_time(matrix):
+    """ Given a boolean-valued matrix of shape (m, n), for each row of the matrix
+    find the largest index storing a True value.
+
+    Parameters
+    -----------
+    matrix : array
+        Boolean array of shape (m, n)
+
+    Returns
+    --------
+    quenching_index : array
+        Length-m array of integers, each between [0, n-1], inclusive.
+    """
+    assert len(np.shape(matrix)) == 2, "Input ``matrix`` must be a 2-d ndarray"
+    return (matrix.shape[1] - 1) - np.argmin(matrix[::-1], axis=1)
