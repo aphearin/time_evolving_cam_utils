857667ef36c3369ed46b619f5b88c99421e7c902
Andrew Hearin <andrew.hearin@yale.edu>
2016-08-19 18:17:25 -0400

    partial fix to the still-buggy quenching_time function. New failing test test_quenching_time3 shows undesired edge case behavior.

--- quenching_time.py
+++ quenching_time.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def quenching_time(matrix):
+def quenching_indices(matrix):
     """ Given a boolean-valued matrix of shape (m, n), for each row of the matrix
     find the largest index storing a True value.
 
@@ -18,4 +18,4 @@ def quenching_time(matrix):
         Length-m array of integers, each between [0, n-1], inclusive.
     """
     assert len(np.shape(matrix)) == 2, "Input ``matrix`` must be a 2-d ndarray"
-    return (matrix.shape[1] - 1) - np.argmin(matrix[::-1], axis=1)
+    return (matrix.shape[1] - 1) - np.argmin(matrix[:, ::-1], axis=1)
--- test_quenching_time.py
+++ test_quenching_time.py
@@ -1,7 +1,44 @@
 """
 """
 import numpy as np
+from quenching_time import quenching_indices
 
 
+def test_quenching_time1():
+    """
+    """
+    m = np.array(((-10, -10, -12, -12), (-10, -12, -12, -12)))
+    bm = m < -11
+    qtimes = quenching_indices(bm)
+    assert np.all(qtimes == (1, 0)), qtimes
 
+
+def test_quenching_time2():
+    """
+    """
+    m = np.array((
+        (-10, -10.5, -11.5, -11.25, -10, -12, -12),
+        (-11.25, -10.5, -11.25, -11.5, -10.9, -12, -12),
+        (-10, -10.5, -11.5, -11.25, -10, -12, -12)
+    ))
+
+    bm = m < -11
+    qtimes = quenching_indices(bm)
+
+    assert np.all(qtimes == (4, 4, 4)), qtimes
+
+
+def test_quenching_time3():
+    """
+    """
+    m = np.array((
+        (-12, -12, -12, -12, -12, -12, -12),
+        (-10, -10, -10, -10, -10, -10, -10),
+        (-11, -11, -11, -11, -11, -11, -11),
+    ))
+
+    bm = m <= -11
+    qtimes = quenching_indices(bm)
+
+    assert np.all(qtimes == (0, 6, 0)), qtimes
 
