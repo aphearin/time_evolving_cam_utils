36193f4f201f185ce2ca373608c0d0e1933289a6
Andrew Hearin <andrew.hearin@yale.edu>
2016-08-19 09:21:43 -0400

    Added function to create an hdf5 file storing a joint collation of sfh and iclh histories formatted as multi-d arrays in the same table. Result is stored in the following file:
    sfh_catalog_1.002310_smcut_9.50_formatted_histories.hdf5
    
    File currently exists in the following two directories:
    /Users/aphearin/Dropbox/UniverseMachine/data/histories/prelim_sfh_reduction/

--- reformat_smcut_txt_catalogs.py
+++ reformat_smcut_txt_catalogs.py
@@ -2,6 +2,7 @@
 reduce_sfh_ascii.py script into an hdf5 binary with
 columns that have multi-dimensional arrays of formation history information.
 """
+import os
 import numpy as np
 from astropy.table import Table
 from read_sfh import get_history_indices, get_num_scales
@@ -12,8 +13,8 @@ def history_ascii_to_hdf5(ascii_fname, output_fname, history='sfh'):
     """
     t = Table.read(ascii_fname, format='ascii.commented_header')
 
-    sfh_keylist = list(key for key in t.keys() if key[0:3]==history)
-    remaining_keylist = list(key for key in t.keys() if key[0:3]!=history)
+    sfh_keylist = list(key for key in t.keys() if history in key)
+    remaining_keylist = list(key for key in t.keys() if history not in key)
 
     m = np.zeros((len(t), len(sfh_keylist)))
     for i in range(len(sfh_keylist)):
@@ -24,8 +25,21 @@ def history_ascii_to_hdf5(ascii_fname, output_fname, history='sfh'):
         result[key] = t[key]
     result[history] = m
 
+    result.sort('id')
     result.write(output_fname, path='data')
 
+
+def produce_collated_history_hdf5(sfh_ascii_fname, iclh_ascii_fname, output_fname):
+    """
+    """
+    history_ascii_to_hdf5(sfh_ascii_fname, 'tmp.sfh.hdf5', history='sfh')
+    history_ascii_to_hdf5(iclh_ascii_fname, 'tmp.iclh.hdf5', history='iclh')
+    t1 = Table.read('tmp.sfh.hdf5', path='data')
+    t2 = Table.read('tmp.iclh.hdf5', path='data')
+    t1['iclh'] = t2['iclh']
+    t1.write(output_fname, path='data')
+    os.remove('tmp.sfh.hdf5')
+    os.remove('tmp.iclh.hdf5')
 
 
 
