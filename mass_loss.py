9837319cd33594259accb8429984a68b26adbffe
Andrew Hearin <andrew.hearin@yale.edu>
2016-08-19 14:43:51 -0400

    Added ssfr_history function

--- mass_loss.py
+++ mass_loss.py
@@ -23,3 +23,46 @@ def redshift_zero_stellar_mass(sfr_histories):
     z0_stellar_mass = np.sum(integrand*universe_dt_array, axis=1)*1e9
 
     return z0_stellar_mass
+
+
+def stellar_mass_t_now(sfr_histories, t_now):
+    """
+    """
+    shape_err_msg = "Input ``sfr_histories`` must have shape (num_gals, num_time_steps)"
+    assert len(np.shape(sfr_histories)) == 2, shape_err_msg
+
+    idx_nearest_time = min(
+        np.searchsorted(universe_age_array, t_now) + 1, sfr_histories.shape[1]-1)
+
+    frac_loss = mass_loss_fraction(
+        universe_age_array[idx_nearest_time] - universe_age_array[:idx_nearest_time])
+    integrand = sfr_histories[:, :idx_nearest_time] * (1. - frac_loss)
+
+    return np.sum(integrand*universe_dt_array[:idx_nearest_time], axis=1)*1e9
+
+
+def ssfr_t_now(sfr_histories, t_now):
+    """
+    """
+    shape_err_msg = "Input ``sfr_histories`` must have shape (num_gals, num_time_steps)"
+    assert len(np.shape(sfr_histories)) == 2, shape_err_msg
+
+    idx_nearest_time = min(
+        np.searchsorted(universe_age_array, t_now) + 1, sfr_histories.shape[1]-1)
+
+    frac_loss = mass_loss_fraction(
+        universe_age_array[idx_nearest_time] - universe_age_array[:idx_nearest_time])
+    integrand = sfr_histories[:, :idx_nearest_time] * (1. - frac_loss)
+
+    sm = np.sum(integrand*universe_dt_array[:idx_nearest_time], axis=1)*1e9
+
+    result = np.zeros_like(sm)
+    mask = np.flatnonzero(sm)
+    result[mask] = sfr_histories[mask, idx_nearest_time]/sm[mask]
+    return result
+
+
+
+
+
+
