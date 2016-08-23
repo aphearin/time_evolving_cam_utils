22aff30beaa6a128d1bbfc8e029a3927d0c5d4b5
Andrew Hearin <andrew.hearin@yale.edu>
2016-08-23 10:10:04 -0400

    Added mass_loss_fraction function to history_functions module

--- history_functions.py
+++ history_functions.py
@@ -5,6 +5,23 @@ also summary statistics like quenching times.
 import numpy as np
 
 
+def mass_loss_fraction(dt):
+    """
+    Fraction of mass lost during time interval dt due to passive evolution.
+
+    Parameters
+    ----------
+    dt : array
+        Length N array of time intervals in units of Gyr
+
+    Returns
+    --------
+    frac_loss : array
+        Length N array of fractional mass loss.
+    """
+    return 0.05*np.log(1 + 1000.*dt/1.4)
+
+
 def stellar_mass_history(sfr_history, cosmic_age):
     """
     Parameters
@@ -24,8 +41,15 @@ def stellar_mass_history(sfr_history, cosmic_age):
     sm_history : array
         Array of shape (num_gals, num_time_steps) storing the value of M*
         in units of Msun for each galaxy as a function of cosmic time.
-        Algorithm assumes that all galaxies begin with zero stellar mass
+        Algorithm is an implementation of Eq. 17 in Behroozi+17,
+        assumes that all galaxies begin with zero stellar mass
         and that mass loss is calculated according to the Behroozi+13 fitting function.
     """
-    pass
+    dt_in_Gyr = np.diff(cosmic_age)
+
+
+
+
+
+
 
