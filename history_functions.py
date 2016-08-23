6b207c95f3b78a4189b89331f3de04eb98a40df0
Andrew Hearin <andrew.hearin@yale.edu>
2016-08-23 18:12:00 -0400

    Finished rewriting stellar_mass_t_now function. Version in history_functions module agrees with Behroozi to better than 1% for all objects.

--- history_functions.py
+++ history_functions.py
@@ -8,57 +8,39 @@ fname="/Users/aphearin/Dropbox/UniverseMachine/data/histories/prelim_sfh_reducti
 bolplanck_ages = np.load(fname)
 
 
-def mass_loss_fraction(dt):
+def remaining_mass_fraction(t, t_form):
     """
     Fraction of mass lost during time interval dt due to passive evolution.
 
     Parameters
     ----------
-    dt : array
-        Length N array of time intervals in units of Gyr
+    t : float
+        Age of universe at the time for which the remaining mass fraction
+        is to be calculated, in units of Gyr.
+
+    t_form : array_like
+        Length N array of times at which the stars were formed, in units of Gyr.
+        All values of t_form should be less than t.
 
     Returns
     --------
-    frac_loss : array
-        Length N array of fractional mass loss.
+    mass_fraction : array
+        Length N array of the fraction of remaining mass.
     """
-    return 0.05*np.log(1 + 1000.*dt/1.4)
+    return 1 - 0.05*np.log(1 + 1000.*(t - t_form)/1.4)
 
 
-def stellar_mass_history(sfr_history, cosmic_age_array=bolplanck_ages):
+def stellar_mass_t_now(sfr_history, t_now, cosmic_age_array=bolplanck_ages):
     """
-    Calculate stellar mass histories from star formation histories.
-
-    Parameters
-    ----------
-    sfr_history : array
-        Array of shape (num_gals, num_time_steps) storing the value of SFR
-        in units of Msun/yr for each galaxy as a function of cosmic time.
-
-    cosmic_age_array : array
-        Array of shape (num_time_steps, ) storing the values of the age of the
-        universe at which galaxy SFR has been tabulated, in units of Gyr.
-        Sign and normalization convention are such that galaxies have
-        zero stellar mass at cosmic_age_array[0], with the present-day
-        cosmic epoch being stored in cosmic_age_array[-1]
-
-    Returns
-    --------
-    sm_history : array
-        Array of shape (num_gals, num_time_steps) storing the value of M*
-        in units of Msun for each galaxy as a function of cosmic time.
-        Algorithm is an implementation of Eq. 17 in Behroozi+17,
-        assumes that all galaxies begin with zero stellar mass
-        and that mass loss is calculated according to the Behroozi+13 fitting function.
     """
-    dt_in_gyr = np.diff(cosmic_age_array)
-    frac_loss = mass_loss_fraction(dt_in_gyr)
-    integrand = sfr_history[:, :-1]*dt_in_gyr*(1.-frac_loss)
-    return np.insert(np.cumsum(integrand, axis=1), 0, 0, axis=1)*1e9
+    assert t_now > cosmic_age_array[0], "This function does not work for t_now < t_init"
+    assert t_now < cosmic_age_array[-1], "This function does not work for t_now > today"
 
+    idx_t_now = np.searchsorted(cosmic_age_array, t_now)
+    sfh_matrix = np.insert(sfr_history[:, :idx_t_now+1], 0, sfr_history[:, 0], axis=1)
+    t = np.append(np.insert(cosmic_age_array[:idx_t_now], 0, 0), t_now)
 
-
-
-
-
-
+    sfh_midpoints_matrix = 0.5*(sfh_matrix[:, 1:] + sfh_matrix[:, :-1])
+    dt = np.diff(t)
+    frac_remaining = np.append(remaining_mass_fraction(t_now, t[1:-1]), 1.)
+    return np.sum(sfh_midpoints_matrix * dt * frac_remaining, axis=1) * 1e9
