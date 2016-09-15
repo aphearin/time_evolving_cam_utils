2371565b85e620d96acf75c1a89bbc826cf381e1
Andrew Hearin <andrew.hearin@yale.edu>
2016-09-15 11:23:47 -0400

    Added satellite_lookup_table_indices function to satellite_quenching_efficiency

--- satellite_quenching_efficiency.py
+++ satellite_quenching_efficiency.py
@@ -22,3 +22,14 @@ def build_matching_central_lookup_table(central_sm_histories, central_is_quenche
     return lookup_table
 
 
+def satellite_lookup_table_indices(satellite_sm_histories, satellite_time_indices, logsm_bins):
+    num_satellites, num_time_steps = satellite_sm_histories.shape
+    msg = "satellite_sm_histories and satellite_time_indices and have inconsistent shapes"
+    assert num_satellites == len(satellite_time_indices), msg
+
+    satellite_sm = satellite_sm_histories[np.arange(num_satellites), satellite_time_indices]
+    satellite_sm_idx = np.digitize(satellite_sm, logsm_bins)
+
+    satellite_sm_idx[satellite_sm_idx >= len(logsm_bins)-1] = len(logsm_bins)-2
+
+    return np.array((satellite_sm_idx, satellite_time_indices))
--- test_satellite_quenching_efficiency.py
+++ test_satellite_quenching_efficiency.py
@@ -2,6 +2,7 @@
 """
 import numpy as np
 from satellite_quenching_efficiency import build_matching_central_lookup_table
+from satellite_quenching_efficiency import satellite_lookup_table_indices
 from time import time
 
 
@@ -20,4 +21,25 @@ def test_build_matching_central_lookup_table():
     assert runtime < 10
 
     assert np.all(table[~np.isnan(table)] == 1)
+
+
+def test_satellite_lookup_table_indices():
+    num_centrals, num_time_steps = 1000, 18
+    logsm_min, logsm_max, num_sm_bins = 8, 11, 50
+    logsm_bins = np.linspace(logsm_min, logsm_max, num_sm_bins)
+
+    central_sm_histories = np.random.uniform(10**logsm_min, 10**logsm_max, num_centrals*num_time_steps)
+    central_sm_histories = np.reshape(central_sm_histories, (num_centrals, num_time_steps))
+    central_is_quenched = np.random.randint(0, 2, num_centrals).astype(bool)
+    lookup_table = build_matching_central_lookup_table(central_sm_histories, central_is_quenched, logsm_bins)
+
+    num_satellites = 500
+    satellite_sm_histories = np.random.uniform(10**logsm_min, 10**logsm_max, num_satellites*num_time_steps)
+    satellite_sm_histories = np.reshape(satellite_sm_histories, (num_satellites, num_time_steps))
+    satellite_time_indices = np.random.randint(0, num_time_steps, num_satellites)
+
+    satellite_idx = satellite_lookup_table_indices(
+        satellite_sm_histories, satellite_time_indices, logsm_bins)
+
+    matching_vals = lookup_table[satellite_idx]
 
